4534b00aa15fd485d2e3e3124852e0d29c6f6562
Yusuf7066 <eklife_88@hotmail.com>
2025-04-10 02:11:26 +0300

    ✅ Fix dark mode button

--- static/js/theme.js
+++ static/js/theme.js
@@ -2,8 +2,16 @@ function toggleTheme() {
   document.body.classList.toggle('dark');
   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 }
+
 window.onload = () => {
+  // Apply stored theme
   if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark');
+  }
+
+  // Attach toggle function to button
+  const toggleButton = document.getElementById('darkToggle');
+  if (toggleButton) {
+    toggleButton.addEventListener('click', toggleTheme);
   }
 };
