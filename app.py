86fe319c39419a0eabdee6c58ffc01e71e6c573d
Yusuf7066 <eklife_88@hotmail.com>
2025-04-19 00:58:33 +0300

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -195,5 +195,7 @@ register_edit_prediction_route(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
+
 
